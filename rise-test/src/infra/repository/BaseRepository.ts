a66570c22d0cbe795438da8d92495386d54b9d8f
yvprosper <prosper@youverify.co>
2023-08-27 08:06:19 +0100

    fix: get all query

--- rise-test/src/infra/repository/BaseRepository.ts
+++ rise-test/src/infra/repository/BaseRepository.ts
@@ -5,7 +5,11 @@ class BaseRepository {
   }
 
   getAllValuesFrom(table_: string): string {
-    return `select * from ${table_}`;
+    let selectedColumns: string[];
+    table_ === "users"
+      ? (selectedColumns = ["id", "first_name", "last_name", "email", "gender", "created_on"])
+      : (selectedColumns = ["*"]);
+    return `SELECT ${selectedColumns!.join(", ")} FROM ${table_}`;
   }
 
   getByCol() {
